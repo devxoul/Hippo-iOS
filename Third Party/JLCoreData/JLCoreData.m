2c556f5d3f670dc6d36f459f5119cac0d560b297
전수열 <devxoul@gmail.com>
2013-09-19 20:19:34 +0900

    JLCoreData momd logic

--- Third Party/JLCoreData/JLCoreData.m
+++ Third Party/JLCoreData/JLCoreData.m
@@ -58,8 +58,7 @@
     if (_managedObjectModel != nil) {
         return _managedObjectModel;
     }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Hippo" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    _managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:nil];
     return _managedObjectModel;
 }
 
