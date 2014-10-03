d03856ab9aed3e0687536cebe58ea62f176142e4
Suyeol Jeon <devxoul@gmail.com>
2014-10-03 16:09:22 +0900

    Add SSBouncyButton

--- Hippo-Bridging-Header.h
+++ Hippo-Bridging-Header.h
@@ -2,4 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "UIView+Positioning/UIView+Positioning.h"
+#import <SSBouncyButton/SSBouncyButton.h>
+#import <UIView+Positioning/UIView+Positioning.h>
