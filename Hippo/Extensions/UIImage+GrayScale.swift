85010b067266ca2bbb98013f989eae1dbc3f0025
arnaudlamy <arnaud.lamy@gmail.com>
2016-06-29 13:32:37 +0200

    Support swift 2

--- Hippo/Extensions/UIImage+GrayScale.swift
+++ Hippo/Extensions/UIImage+GrayScale.swift
@@ -3,6 +3,7 @@
 //  Hippo
 //
 //  Created by 전수열 on 10/9/14.
+//  Updated by Arnaud Lamy on 29/06/2016 to support Swift 2
 //  Copyright (c) 2014 Joyfl. All rights reserved.
 //
 
@@ -13,13 +14,13 @@ extension UIImage {
         let imageRect = CGRectMake(0, 0, self.size.width, self.size.height);
         let colorSpace = CGColorSpaceCreateDeviceGray();
 
-        let width = UInt(self.size.width)
-        let height = UInt(self.size.height)
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
         let context = CGBitmapContextCreate(nil, width, height, 8, 0, colorSpace, .allZeros);
         CGContextDrawImage(context, imageRect, self.CGImage!);
 
         let imageRef = CGBitmapContextCreateImage(context);
-        let newImage = UIImage(CGImage: imageRef)
+        let newImage = UIImage(CGImage: imageRef!)
         return newImage
     }
 }
